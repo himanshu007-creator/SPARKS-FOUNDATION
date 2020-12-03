93c2e9ac6895b3c710e4dee50adf0291ab4ec10d
himanshu007-creator <65963997+himanshu007-creator@users.noreply.github.com>
2020-12-03 18:00:33 +0530

    Add files via upload

---
+++ firebaseConst.js
@@ -0,0 +1,9 @@
+const dataFire = {
+    apiKey: "AIzaSyD29bOwp-em6Z_X1i0iXlq73a-TlgAjHVA",
+    authDomain: "coderush-df262.firebaseapp.com",
+    databaseURL: "https://coderush-df262.firebaseio.com",
+    projectId: "coderush-df262",
+    storageBucket: "coderush-df262.appspot.com",
+    messagingSenderId: "485814735018"
+  }
+  export { dataFire};
